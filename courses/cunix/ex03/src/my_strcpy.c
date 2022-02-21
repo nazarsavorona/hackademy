b5727e69ca71f990a3fd29d2225f625c97f3a520
Nazarii <nazar.savorona@gmail.com>
2022-02-21 12:31:27 +0000

    Add ex03 solution

---
+++ courses/cunix/ex03/src/my_strcpy.c
@@ -0,0 +1,12 @@
+char *my_strcpy(char *dest, const char *src) {
+    const char* current_src = src;
+    char* current_dest = dest;
+    
+    while(*current_src != '\0') {
+        *(current_dest++) = *(current_src++);
+    }
+
+    *(current_dest++) = '\0';
+
+    return dest;
+}
